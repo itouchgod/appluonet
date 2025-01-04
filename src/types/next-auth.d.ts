09b30abd47bf30873f385a43367c59b216c26e04
itouchgod <itouchgod@gmail.com>
2025-01-04 15:34:04 +0800

    0.0.0.3

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,11 @@
-import NextAuth from 'next-auth';
+import { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
   interface Session {
-    user: {
+    user?: {
       id: string;
       username: string;
-    };
+    } & DefaultSession['user'];
   }
 
   interface User {
