a8875051c69ef17adf9ad6934de35a541189b0c6
itouchgod <itouchgod@gmail.com>
2025-03-02 19:31:22 +0800

    V5.0.2ooooooooooook

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1471,15 +1471,9 @@ Beneficiary: Luo & Company Co., Limited`,
 
                 <button
                   type="button"
-                  onClick={async (e) => {
-                    e.preventDefault();
+                  onClick={async () => {
                     try {
-                      const pdfUrl = await generateInvoicePDF({
-                        ...invoiceData,
-                        showPaymentTerms,
-                        additionalPaymentTerms,
-                        templateConfig
-                      }, true);
+                      const pdfUrl = await generateInvoicePDF(invoiceData, true);
                       if (pdfUrl) {
                         setPreviewUrl(pdfUrl.toString());
                         setShowPreview(true);
@@ -1502,7 +1496,7 @@ Beneficiary: Luo & Company Co., Limited`,
                     <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     <path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                   </svg>
-                  Preview Invoice
+                  Preview
                 </button>
               </div>
             </form>
