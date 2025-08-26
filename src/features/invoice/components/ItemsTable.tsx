69b361c2600ad14dd4d01c468b356e15993a7239
itouchgod <itouchgod@gmail.com>
2025-08-26 21:49:24 +0800

    V25.8.26.0.5

--- src/features/invoice/components/ItemsTable.tsx
+++ src/features/invoice/components/ItemsTable.tsx
@@ -364,7 +364,7 @@ export const ItemsTable = React.memo(() => {
               <svg className="w-4 h-4 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              提示：双击单元格可以切换红色高亮显示
+              双击切换红色高亮
             </div>
           </div>
         </div>
